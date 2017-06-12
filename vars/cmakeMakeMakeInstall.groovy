a5e47069a74d0e0e2e6ed4bacc5d4bf34638399d
Jens Ivar Jørdre <jeij@statoil.com>
2017-06-12 10:42:32 +0200

    Add step cmakeMakeMakeInstall

---
+++ vars/cmakeMakeMakeInstall.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+
+def call(Map<String, Object> args) {
+    def cacheFile = args.containsKey('cacheFile') ? args.cacheFile : null
+    def modules = args.containsKey('modules') ? args.modules : null
+    def makeJobs = args.containsKey('makeJobs') ? args.makeJobs : null
+
+    def cmdLine = "set -e"
+
+    if (modules) {
+        for (def module : modules) {
+            def source = module.source
+            def name = module.name
+            cmdLine += " && source ${source} && module add ${name}"
+        }
+    }
+
+    cmdLine += " && source /project/res/SDP_bashrc"
+    cmdLine += " && cmake" + (cacheFile ? " -C ${cacheFile} " : "") + " .."
+    cmdLine += " && make" + (makeJobs ? " -j ${makeJobs} " : "")
+    cmdLine += " && make install"
+    sh cmdLine
+}
